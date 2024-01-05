fda819c7423aeb6d8623692be0ecc1fd983b5d84
Kamil Zaremba <kamz190@gmail.com>
2024-01-05 19:19:12 +0100

    remove data check

--- reg_omp.c
+++ reg_omp.c
@@ -6,24 +6,17 @@
 #include <math.h>
 #include <omp.h>
 
-
 void simpleLinearRegression(double **data, int numRows, int numCols, double *slope, double *intercept, double *rSquared, double *mse, double *mae);
 
-int main() {
+int main()
+{
     const char *filename = "prir_data.csv";
-    
+
     double **data;
     int numRows, numCols;
 
     // Loading data from CSV
     loadCSV(filename, &data, &numRows, &numCols);
-    
-    // Checking the data
-    printf("Number of rows: %d\n", numRows);
-    printf("Number of columns: %d\n", numCols);
-    printf("First 5 rows:\n");
-    printData(data, 5, numCols);
-
 
     double slope, intercept, rSquared, mse, mae;
 
@@ -31,7 +24,6 @@ int main() {
     double startTime = omp_get_wtime();
     simpleLinearRegression(data, numRows, numCols, &slope, &intercept, &rSquared, &mse, &mae);
     double endTime = omp_get_wtime();
-
 
     // Display the results
     printf("\n\nResults\n");
@@ -45,17 +37,21 @@ int main() {
     printf("Execution Time: %.4f seconds\n", endTime - startTime);
 
     // Deallocating the memory
-    for (int i = 0; i < numRows; i++) { free(data[i]); }
+    for (int i = 0; i < numRows; i++)
+    {
+        free(data[i]);
+    }
     free(data);
     return 0;
 }
 
-
-void simpleLinearRegression(double **data, int numRows, int numCols, double *slope, double *intercept, double *rSquared,  double *mse, double *mae) {
+void simpleLinearRegression(double **data, int numRows, int numCols, double *slope, double *intercept, double *rSquared, double *mse, double *mae)
+{
     double sumX = 0.0, sumY = 0.0, sumXY = 0.0, sumX2 = 0.0;
 
-    #pragma omp parallel for reduction(+:sumX,sumY,sumXY,sumX2)
-    for (int i = 0; i < numRows; i++) {
+#pragma omp parallel for reduction(+ : sumX, sumY, sumXY, sumX2)
+    for (int i = 0; i < numRows; i++)
+    {
         sumX += data[i][1];
         sumY += data[i][0];
         sumXY += data[i][1] * data[i][0];
@@ -72,8 +68,9 @@ void simpleLinearRegression(double **data, int numRows, int numCols, double *slo
     double ssr = 0.0; // Regression sum of squares
     double sst = 0.0; // Total sum of squares
 
-    #pragma omp parallel for reduction(+:ssr,sst)
-    for (int i = 0; i < numRows; i++) {
+#pragma omp parallel for reduction(+ : ssr, sst)
+    for (int i = 0; i < numRows; i++)
+    {
         double yPredicted = *intercept + *slope * data[i][1];
         ssr += (yPredicted - yMean) * (yPredicted - yMean);
         sst += (data[i][0] - yMean) * (data[i][0] - yMean);
@@ -85,13 +82,14 @@ void simpleLinearRegression(double **data, int numRows, int numCols, double *slo
     double locMSE = 0.0;
     double locMAE = 0.0;
 
-    #pragma omp parallel for reduction(+:locMSE,locMAE)
-    for (int i = 0; i < numRows; i++) {
+#pragma omp parallel for reduction(+ : locMSE, locMAE)
+    for (int i = 0; i < numRows; i++)
+    {
         double yPredicted = *intercept + *slope * data[i][1];
         locMSE += pow(data[i][0] - yPredicted, 2);
         locMAE += fabs(data[i][0] - yPredicted);
     }
 
-    *mse = locMSE  / n;
+    *mse = locMSE / n;
     *mae = locMAE / n;
 }
--- sequential.c
+++ sequential.c
@@ -5,24 +5,17 @@
 #include <time.h>
 #include <math.h>
 
-
 void simpleLinearRegression(double **data, int numRows, int numCols, double *slope, double *intercept, double *rSquared, double *mse, double *mae);
 
-int main() {
+int main()
+{
     const char *filename = "prir_data.csv";
-    
+
     double **data;
     int numRows, numCols;
 
     // Loading data from CSV
     loadCSV(filename, &data, &numRows, &numCols);
-    
-    // Checking the data
-    printf("Number of rows: %d\n", numRows);
-    printf("Number of columns: %d\n", numCols);
-    printf("First 5 rows:\n");
-    printData(data, 5, numCols);
-
 
     double slope, intercept, rSquared, mse, mae;
 
@@ -30,7 +23,6 @@ int main() {
     clock_t start = clock();
     simpleLinearRegression(data, numRows, numCols, &slope, &intercept, &rSquared, &mse, &mae);
     clock_t end = clock();
-
 
     // Display the results
     printf("\n\nResults\n");
@@ -41,20 +33,24 @@ int main() {
     printf("MAE(Mean Absolute Error): %.4f\n", mae);
 
     // Display execution time
-    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("Execution Time: %.4f seconds\n", cpu_time_used);
 
     // Deallocating the memory
-    for (int i = 0; i < numRows; i++) { free(data[i]); }
+    for (int i = 0; i < numRows; i++)
+    {
+        free(data[i]);
+    }
     free(data);
     return 0;
 }
 
-
-void simpleLinearRegression(double **data, int numRows, int numCols, double *slope, double *intercept, double *rSquared,  double *mse, double *mae) {
+void simpleLinearRegression(double **data, int numRows, int numCols, double *slope, double *intercept, double *rSquared, double *mse, double *mae)
+{
     double sumX = 0.0, sumY = 0.0, sumXY = 0.0, sumX2 = 0.0;
 
-    for (int i = 0; i < numRows; i++) {
+    for (int i = 0; i < numRows; i++)
+    {
         sumX += data[i][1];
         sumY += data[i][0];
         sumXY += data[i][1] * data[i][0];
@@ -71,7 +67,8 @@ void simpleLinearRegression(double **data, int numRows, int numCols, double *slo
     double ssr = 0.0; // Regression sum of squares
     double sst = 0.0; // Total sum of squares
 
-    for (int i = 0; i < numRows; i++) {
+    for (int i = 0; i < numRows; i++)
+    {
         double yPredicted = *intercept + *slope * data[i][1];
         ssr += (yPredicted - yMean) * (yPredicted - yMean);
         sst += (data[i][0] - yMean) * (data[i][0] - yMean);
@@ -83,7 +80,8 @@ void simpleLinearRegression(double **data, int numRows, int numCols, double *slo
     *mse = 0.0;
     *mae = 0.0;
 
-    for (int i = 0; i < numRows; i++) {
+    for (int i = 0; i < numRows; i++)
+    {
         double yPredicted = *intercept + *slope * data[i][1];
         *mse += pow(data[i][0] - yPredicted, 2);
         *mae += fabs(data[i][0] - yPredicted);
